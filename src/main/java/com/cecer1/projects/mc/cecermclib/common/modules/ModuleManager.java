3d56fbe3c259ae0819d4e65d370d4cece5afb6a8
Cecer <cecer@hypixel.net>
2022-01-29 23:08:38 +0000

    Add extra logging to ModuleManager

--- src/main/java/com/cecer1/projects/mc/cecermclib/common/modules/ModuleManager.java
+++ src/main/java/com/cecer1/projects/mc/cecermclib/common/modules/ModuleManager.java
@@ -5,6 +5,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("RedundantStringFormatCall") // Because some environments break printf
 public final class ModuleManager {
 
     private final Map<Class<? extends IModule>, IModule> modules;
@@ -30,7 +31,6 @@ public final class ModuleManager {
     /**
      * Registers a module as a service module.
      */
-    @SuppressWarnings("RedundantStringFormatCall") // Because some environment break printf
     <TService extends IModule, TModule extends TService> ModuleManager registerServiceModule(Class<TService> serviceClass, TModule module) {
         Class<? extends IModule> moduleClass = module.getClass();
 
@@ -70,16 +70,31 @@ public final class ModuleManager {
         }
         this.registrationStarted = true;
 
+        System.out.println("[CecerMCLib] [ModuleManager] Registering modules...");
         ModuleRegistrationCallback.EVENT.invoker().handle(new ModuleRegistrationCallback.RegistrationContext(this));
+        System.out.println("[CecerMCLib] [ModuleManager] Modules registered: " + this.modules.size());
         
+        System.out.println("[CecerMCLib] [ModuleManager] Checking module dependencies...");
+        boolean failed = false;
         for (IModule module : this.modules.values()) {
             for (Class<? extends IModule> dependency : module.getDependencies()) {
                 if (!this.modules.containsKey(dependency)) {
-                    throw new IllegalStateException(String.format("Missing module dependency \"%s\" for \"%s\"", dependency.getName(), module.getClass()));
+                    System.err.println(String.format("[CecerMCLib] [ModuleManager] Missing module dependency \"%s\" for \"%s\"!", dependency.getName(), module.getClass()));
+                    failed = true;
                 }
             }
         }
+        if (failed) {
+            throw new IllegalStateException("Missing dependencies!");
+        }
+        System.out.println("[CecerMCLib] [ModuleManager] Module dependencies met!");
+
+        System.out.println("[CecerMCLib] [ModuleManager] Invoking AllModulesRegisteredCallback...");
         AllModulesRegisteredCallback.EVENT.invoker().handle();
+        
+        System.out.println("[CecerMCLib] [ModuleManager] Invoking AllModulesInitialisedCallback...");
         AllModulesInitialisedCallback.EVENT.invoker().handle();
+        
+        System.out.println("[CecerMCLib] [ModuleManager] Module registration complete!");
     }
 }
